0660c3909c3dd9323014828f35348f304ecce7b1
Paul Maxwell <30607551+paulcmaxwell@users.noreply.github.com>
2023-02-08 16:33:17 -0500

    Second Commit

---
+++ utils.supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export default createClient(
+  process.env.https://qugibpllwklxeouadndd.supabase.co,
+  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1Z2licGxsd2tseGVvdWFkbmRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUwOTIwMjgsImV4cCI6MTk5MDY2ODAyOH0.est168hNqsDerfrO3k2Th-TfrluJe1KeR6eTnDArsaM
+);
